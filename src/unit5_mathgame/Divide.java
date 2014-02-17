f6898eab3112097dc2b8e777d6c1525020fee696
Dad <Dad@ExpressRealty.PK5001Z>
2014-02-16 20:36:40 -0600

    Updated the Divide Math Class to implement the IOperation interface

--- src/unit5_mathgame/Divide.java
+++ src/unit5_mathgame/Divide.java
@@ -1,15 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package unit5_mathgame;
 
 /**
+ * 
+ * <p/>
+ * Revision History:<br/>
+ * Name: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Date:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Change:<br/>
+ * ------------------  -------------   ---------<br/>
  *
- * author Brian Bagwell
+ * @author Brian Bagwell
+ * @param <T>
+ * @param <U>
  */
-public class Divide {
+public class Divide<T extends Double, U extends String> extends Operator<T, U> {
+
+    @Override
+    public void result() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isCorrect(T input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public U response(boolean isCorrect) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void playResponse(boolean isCorrect) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
