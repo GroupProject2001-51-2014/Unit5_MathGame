91162d46684e570814e7f39e89591e2a3934d4f1
John Mambo <>
2014-02-23 13:49:10 -0600

    Removed all other method implementation except result() They are abstracted to the abstract Operator class.

--- src/unit5_mathgame/Multiply.java
+++ src/unit5_mathgame/Multiply.java
@@ -1,43 +1,26 @@
 
 package unit5_mathgame;
 
-
-import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
 /**
  * 
  * <p/>
  * Revision History:<br/>
  * Name: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Date:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Change:<br/>
  * ------------------  -------------   ---------<br/>
- *
+ * John Mambo          02/23/2014      Removed all other method implementation except public void {@code result()}<br/>
+ *                                     They are abstracted to the abstract Operator class.
+ * 
  * @author Brian Bagwell
  * @param <T> Template type parameter with {@code Double} upper bound 
  * @param <U> Template type parameter with {@code String} upper bound 
  * 
  */
 public class Multiply<T extends Double, U extends String> extends Operator<T, U> {
-
-    public Multiply (){}
+    
+    public Multiply(){}
     
     @Override
     public void result() throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean isCorrect(T input) throws UnsupportedOperationException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public U response(boolean isCorrect) throws UnsupportedOperationException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void playResponse(boolean isCorrect) throws UnsupportedOperationException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    }//end result()
 }
