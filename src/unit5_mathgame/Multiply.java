1442e205bb42adb91a711fc12bf81b8006d1d92b
GroupProject2001-51-2014 <john.mambo@gmail.com>
2014-02-23 02:15:52 -0600

    Added "throws UnsupportedOperationException" to the Method construction.

--- src/unit5_mathgame/Multiply.java
+++ src/unit5_mathgame/Multiply.java
@@ -21,22 +21,22 @@ public class Multiply<T extends Double, U extends String> extends Operator<T, U>
     public Multiply (){}
     
     @Override
-    public void result() {
+    public void result() throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean isCorrect(T input) {
+    public boolean isCorrect(T input) throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public U response(boolean isCorrect) {
+    public U response(boolean isCorrect) throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void playResponse(boolean isCorrect) {
+    public void playResponse(boolean isCorrect) throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
