3c19f89903ec319f94e1c3618835b5979ab951c5
John Mambo <>
2014-02-23 02:21:58 -0600

    Added "throws UnsupportedOperationException" to the implemented Methods.

--- src/unit5_mathgame/Divide.java
+++ src/unit5_mathgame/Divide.java
@@ -15,28 +15,28 @@ import java.util.concurrent.ConcurrentHashMap;
  * @param <T> Template type parameter with {@code Double} upper bound 
  * @param <U> Template type parameter with {@code String} upper bound 
  */
-public class Divide<T extends Double, U extends String> extends Operator<T, U> {
+public class Divide<T extends Double, U extends String> extends Operator<T, U>{
 
     
     public Divide (){}
     
     @Override
-    public void result() {
+    public void result()  throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean isCorrect(T input) {
+    public boolean isCorrect(T input)  throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public U response(boolean isCorrect) {
+    public U response(boolean isCorrect)  throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void playResponse(boolean isCorrect) {
+    public void playResponse(boolean isCorrect)  throws UnsupportedOperationException{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
