777096875f9f4fe6dc970b7e1f2bf47cb0cda93e
John Mambo <>
2014-02-23 14:01:04 -0600

    1: For ConcurrentHashMap<DefineValue, T> generateValuePair(ConcurrentHashMap<DefineValue, T> valuePair): 	Removed final and 	made it private 2: For ConcurrentHashMap<DefineValue, T> range(ConcurrentHashMap<DefineValue, T> valuePairInput, Level level, Operation operation): 	made it final and 	updated multiplier logic 3: For generateValuePair(): 	Removed final 4: Implemented boolean isCorrect(T input) 5: Implemented response(boolean isCorrect)

--- src/unit5_mathgame/Operator.java
+++ src/unit5_mathgame/Operator.java
@@ -29,7 +29,7 @@ public abstract class Operator<T extends Double, U extends String> implements IO
      *                                  or if {@code DefineValue.LOWERLIMIT} is {@code null}<br/>
      *                                  or if {@code DefineValue.LOWERLIMIT} is greater than the {@code DefineValue.UPPERLIMIT} is {@code null}<br/>
      */
-    public final ConcurrentHashMap<DefineValue, T> generateValuePair(ConcurrentHashMap<DefineValue, T> valuePair) {
+    private ConcurrentHashMap<DefineValue, T> generateValuePair(ConcurrentHashMap<DefineValue, T> valuePair) {
         synchronized(LOCK){
             if (valuePair.get(DefineValue.UPPERLIMIT) == null 
                 || valuePair.get(DefineValue.LOWERLIMIT) == null) {
@@ -58,10 +58,11 @@ public abstract class Operator<T extends Double, U extends String> implements IO
     }
     
     
-    public ConcurrentHashMap<DefineValue, T> range(ConcurrentHashMap<DefineValue, T> valuePairInput, Level level, Operation operation) {
+    public final ConcurrentHashMap<DefineValue, T> range(ConcurrentHashMap<DefineValue, T> valuePairInput, Level level, Operation operation) {
         synchronized(LOCK){
             ConcurrentHashMap<DefineValue, Double> _valuePair = new ConcurrentHashMap<>(1, 3.0f, 3);
-            double _multiplier = Math.pow(10, operation.index()) * Math.pow(10, level.index() - 1);
+            double _multiplier = Math.pow(10, level.index());
+            //double _multiplier = Math.pow(10, operation.index()) * Math.pow(10, level.index() - 1);
             switch(operation){
                 case ADD:
                 case SUBTRACT:  _valuePair.put(DefineValue.UPPERLIMIT, 1d * _multiplier);
@@ -113,23 +114,67 @@ public abstract class Operator<T extends Double, U extends String> implements IO
     }
     
     @Override
-    public final void generateValuePair(){
+    public void generateValuePair(){
         ConcurrentHashMap<DefineValue, T> _generatevaluePair = generateValuePair(this.valuePair);
         this.setConcurrentHashMap(_generatevaluePair);
     }
     
 
     @Override
-    public abstract void result();
+    public abstract void result() throws UnsupportedOperationException;
 
+    /**
+     * Checks if the {@code input} parameter value provide matches the expected {@code DefineValue.ANSWER}
+     * @param input The user input of their answer to the question presented.
+     * @return {@code boolean} - If the user has provided the correct answer then {@code true} else {@code false} 
+     */
     @Override
-    public abstract boolean isCorrect(T input); 
+    public synchronized boolean isCorrect(T input){
+        boolean _isCorrect = false;
+        try{
+            _isCorrect = (this.valuePair.get(DefineValue.ANSWER).toString() == null ? T.valueOf(input).toString() == null : this.valuePair.get(DefineValue.ANSWER).toString().equals(T.valueOf(input).toString()));
+        }
+        catch(Exception ex){
+            // for debuging - normally would handle the error here.
+            System.out.printf("Error occured while creating 'Add': %s%n", ex);
+        }
+        return _isCorrect;
+    }
 
+     /**
+     * Provides an appropriate response to keep the user engaged.
+     * @param isCorrect Is the user input correct?
+     * @return type of <b>U</> class which contains the response message. 
+     *         The upper bound of <b>U</> is of type {@code String}
+     */
     @Override
-    public abstract U response(boolean isCorrect);
+    public synchronized U response(boolean isCorrect){
+        Random _randNumGen = new Random();
+        int _num = _randNumGen.nextInt(5);
+        if (isCorrect) {
+           switch(_num){
+                case 0: return (U)"Very good!";
+                case 1: return (U)"Excellent!";
+                case 2: return (U)"Nice work!";
+                case 3: return (U)"Keep up the good work!";
+                case 4: return (U)"Hoorah!";
+                default: return null; 
+            } 
+        }
+        else{
+            switch(_num){
+                case 0: return (U)"No. Please try again!";
+                case 1: return (U)"Wrong. Try once more!";
+                case 2: return (U)"Don't give up!";
+                case 3: return (U)"No. Keep trying!";
+                case 4: return (U)"Dig in a little deeper. It will pay-off!";
+                default: return null; 
+            }                
+        } 
+    }//end response
     
     @Override
-    public void playResponse(boolean isCorrect){
+    public void playResponse(boolean isCorrect) throws UnsupportedOperationException{
         // Brian: Use this method to provide sound feedback
     }
 }
