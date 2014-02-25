1656d6ba8de1e52f410d1d3a724943531fecb9f4
Brian Bagwell <>
2014-02-25 15:41:08 -0600

    Merged the files upstream

--- src/unit5_mathgame/Multiply.java
+++ src/unit5_mathgame/Multiply.java
@@ -1,4 +1,3 @@
-
 package unit5_mathgame;
 
 /**
@@ -9,6 +8,8 @@ package unit5_mathgame;
  * ------------------  -------------   ---------<br/>
  * John Mambo          02/23/2014      Removed all other method implementation except public void {@code result()}<br/>
  *                                     They are abstracted to the abstract Operator class.
+ * 
+ * Brian Bagwell       02/24/2014       Modified the results method to coincide with the operation of template interface.
  * 
  * @author Brian Bagwell
  * @param <T> Template type parameter with {@code Double} upper bound 
@@ -21,7 +22,31 @@ public class Multiply<T extends Double, U extends String> extends Operator<T, U>
     
     @Override
     public void result() throws UnsupportedOperationException{
-        throw new UnsupportedOperationException("Not supported yet.");
-        // TODO Brian Bagwell: completed the logic here and delete line 24 above^
+        
+        Double _result;
+        
+        if (this.valuePair == null || this.valuePair.isEmpty()) {
+            throw new IllegalArgumentException("Parameter is null or not initialized");
+        }
+        if (this.valuePair.get(DefineValue.UPPERLIMIT) == null 
+            || this.valuePair.get(DefineValue.LOWERLIMIT) == null 
+            || this.valuePair.get(DefineValue.FIRSTVALUE) == null 
+            || this.valuePair.get(DefineValue.SECONDVALUE) == null) {
+            throw new IllegalArgumentException("One of the values in the parameter is null");
+        }
+        if (this.valuePair.get(DefineValue.LOWERLIMIT) > this.valuePair.get(DefineValue.UPPERLIMIT)) {
+            throw new IllegalArgumentException("Lower Limit cannot be greater than the Upper Limit of the range.");
+        }
+        if (this.valuePair.get(DefineValue.FIRSTVALUE) < this.valuePair.get(DefineValue.LOWERLIMIT) || this.valuePair.get(DefineValue.FIRSTVALUE) > this.valuePair.get(DefineValue.UPPERLIMIT)) {
+            throw new IllegalArgumentException("First Value is out of Range.");
+        }
+        if (this.valuePair.get(DefineValue.SECONDVALUE) < this.valuePair.get(DefineValue.LOWERLIMIT) || this.valuePair.get(DefineValue.SECONDVALUE) > this.valuePair.get(DefineValue.UPPERLIMIT)) {
+            throw new IllegalArgumentException("second Value is out of Range.");
+        }
+        
+        _result = this.valuePair.get(DefineValue.FIRSTVALUE) * this.valuePair.get(DefineValue.SECONDVALUE);
+
+        this.valuePair.put(DefineValue.ANSWER, (T)_result);
+
     }//end result()
 }
